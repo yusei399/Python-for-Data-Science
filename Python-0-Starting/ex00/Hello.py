c64e6201d8166af60b8a4107c14beb2721da7925
yusei399 <yusei2021@keio.jp>
2024-03-09 05:15:22 +0900

    finish

---
+++ Python-0-Starting/ex00/Hello.py
@@ -0,0 +1,10 @@
+ft_list = ["Hello", 'World!']
+ft_tuple = ("Hello", 'France!')
+ft_set = {"Hello", "Paris!"}
+ft_dict = {"Hello" : '42Paris!'}
+
+#your code here
+print(ft_list)
+print(ft_tuple)
+print(ft_set)
+print(ft_dict)
