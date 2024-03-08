7d65a6bab49b32217af492dde256168758bb5931
yusei399 <yusei2021@keio.jp>
2024-03-09 06:35:19 +0900

    finish

---
+++ Python-0-Starting/ex04/whatis.py
@@ -0,0 +1,17 @@
+import sys
+
+args = sys.argv
+
+if len(args) != 2:
+    raise AssertionError("more than one argument is provided")
+
+try:
+    num = int(args[1])
+except ValueError:
+    raise AssertionError("argument is not an integer")
+
+if num % 2 == 0:
+    print("I'm Even.")
+else:
+    print("I'm Odd.")
+
