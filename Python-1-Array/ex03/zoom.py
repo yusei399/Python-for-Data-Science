f5e2fcf1909b2d2881a2f6c3e56ffb47dd3dd32e
yusei399 <contact.ikechan@gmail.com>
2025-02-07 14:06:47 +0100

    fix

--- Python-1-Array/ex03/zoom.py
+++ Python-1-Array/ex03/zoom.py
@@ -1,30 +1,61 @@
+import matplotlib
+matplotlib.use("TkAgg")
 import matplotlib.pyplot as plt
 from load_image import load_image
 
+def show_image(img, cmap="gray"):
+    """
+    画像を表示する。
 
-def display_image_with_axes(img, cmap):
+    Args:
+        img (numpy.ndarray): 表示する画像データ（NumPy 配列）。
+        cmap (str, optional): カラーマップ（デフォルトは "gray"）。
+
+    Returns:
+        None
+    """
     plt.imshow(img, cmap=cmap)
     plt.show()
 
+def crop_image(img_array, x_start=100, x_end=500, y_start=450, y_end=850):
+    """
+    指定された範囲で画像を切り抜く。
 
-def zoom(img_array, x_start=100, x_end=500, y_start=450, y_end=850):
-    zoomed_img = img_array[x_start:x_end, y_start:y_end, 0:1]
+    Args:
+        img_array (numpy.ndarray): 元の画像データ（NumPy 配列）。
+        x_start (int, optional): 切り抜き開始位置（X軸方向）。デフォルトは 100。
+        x_end (int, optional): 切り抜き終了位置（X軸方向）。デフォルトは 500。
+        y_start (int, optional): 切り抜き開始位置（Y軸方向）。デフォルトは 450。
+        y_end (int, optional): 切り抜き終了位置（Y軸方向）。デフォルトは 850。
 
-    return zoomed_img
-
+    Returns:
+        numpy.ndarray: 指定範囲を切り抜いた新しい画像データ。
+    """
+    zoom_image = img_array[x_start:x_end, y_start:y_end, 0:1]
+    return zoom_image
 
 def main():
+    """
+    画像を読み込み、サイズを表示し、切り抜いた後に表示する。
+
+    Raises:
+        AssertionError: `load_image` で発生する可能性のあるアサーションエラー。
+        Exception: その他の予期しない例外。
+
+    Returns:
+        None
+    """
     try:
         original_img = load_image("animal.jpeg")
         print("The shape of image is:", original_img.shape)
         print(original_img)
 
-        zoomed_img = zoom(original_img)
+        zoom_image = crop_image(original_img)
         print("New shape after slicing:",
-              zoomed_img.shape, "or", zoomed_img.shape[:2])
-        print(zoomed_img)
+              zoom_image.shape, "or", zoom_image.shape[:2])
+        print(zoom_image)
 
-        display_image_with_axes(zoomed_img, cmap="gray")
+        show_image(zoom_image, cmap="gray")
 
     except AssertionError as e:
         print("AssertionError:", e)
@@ -32,7 +63,6 @@ def main():
         print("Exception:", e)
     except KeyboardInterrupt:
         pass
-
 
 if __name__ == "__main__":
     main()
