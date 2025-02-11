a9b1ab5e88a165b0536cbfd652216ca7c124a954
yusei399 <contact.ikechan@gmail.com>
2025-02-11 14:04:58 +0100

    finish

--- Python-3-OOP/ex02/DiamondTrap.py
+++ Python-3-OOP/ex02/DiamondTrap.py
@@ -1,20 +1,46 @@
 from S1E7 import Baratheon, Lannister
 
 class King(Baratheon, Lannister):
+    """
+    King クラス
+    Baratheon と Lannister の多重継承により、初期状態は Baratheon の属性が継承される。
+    プロパティを使って、目と髪の色を動的に変更できます。
+    """
     def __init__(self, first_name, is_alive=True):
         super().__init__(first_name, is_alive)
 
+    @property
+    def eyes(self):
+        return self.__dict__['eyes']
+
+    @eyes.setter
+    def eyes(self, color):
+        self.__dict__['eyes'] = color
+
+    @property
+    def hairs(self):
+        return self.__dict__['hairs']
+
+    @hairs.setter
+    def hairs(self, color):
+        self.__dict__['hairs'] = color
+
     def set_eyes(self, color):
+        """プロパティを利用して目の色を変更する"""
         self.eyes = color
-    
-    def set_hairs(self,  color):
+
+    def set_hairs(self, color):
+        """プロパティを利用して髪の色を変更する"""
         self.hairs = color
 
     def get_eyes(self):
+        """プロパティを利用して目の色を取得する"""
         return self.eyes
-        
+
     def get_hairs(self):
+        """プロパティを利用して髪の色を取得する"""
         return self.hairs
+
 
 
 if __name__ == "__main__":
--- Python-3-OOP/ex02/S1E7.py
+++ Python-3-OOP/ex02/S1E7.py
@@ -1,47 +1,42 @@
 from S1E9 import Character
 
 class Baratheon(Character):
+    """Baratheon家の代表。."""
+    
     def __init__(self, first_name, is_alive=True):
-
         super().__init__(first_name, is_alive)
         self.family_name = "Baratheon"
         self.eyes = "brown"
         self.hairs = "dark"
+    
+    def die(self):
+        self.is_alive = False
 
-        def die(self):
-            self.is_alive = False
+    def __str__(self):
+        return f"Vector: ('{self.family_name}', '{self.eyes}', '{self.hairs}')"
+
+    def __repr__(self):
+        return f"Vector: ('{self.family_name}', '{self.eyes}', '{self.hairs}')"
+
 
 class Lannister(Character):
+    """Lannister家の代表。"""
+    
     def __init__(self, first_name, is_alive=True):
         super().__init__(first_name, is_alive)
         self.family_name = "Lannister"
         self.eyes = "blue"
         self.hairs = "light"
-
+    
     def die(self):
         self.is_alive = False
-    # decorator
-    @classmethod 
-    def create_lannister(cls, first_name, is_alive):
-        generate_instance = cls(first_name)
-        generate_instance.is_alive = is_alive
-        return generate_instance
 
+    def __str__(self):
+        return f"Vector: ('{self.family_name}', '{self.eyes}', '{self.hairs}')"
 
-# if __name__ == '__main__':
-#     Robert = Baratheon("Robert")
-#     print(Robert.__dict__)
-#     print(Robert.__str__)
-#     print(Robert.__repr__)
-#     print(Robert.is_alive)
-#     Robert.die()
-#     print(Robert.is_alive)
-#     print(Robert.__doc__)
-#     print("---")
-#     Cersei = Lannister("Cersei")
-#     print(Cersei.__dict__)
-#     print(Cersei.__str__)
-#     print(Cersei.is_alive)
-#     print("---")
-#     Jaine = Lannister.create_lannister("Jaine", True)
-#     print(f"Name : {Jaine.first_name, type(Jaine).__name__}, Alive : {Jaine.is_alive}")
+    def __repr__(self):
+        return f"Vector: ('{self.family_name}', '{self.eyes}', '{self.hairs}')"
+    
+    @classmethod
+    def create_lannister(cls, first_name, is_alive=True):
+        return cls(first_name, is_alive)
