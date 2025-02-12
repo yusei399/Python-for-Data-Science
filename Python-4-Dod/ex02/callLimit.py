626287e800e123d2d87bc58475e5fca3dddd9962
yusei399 <contact.ikechan@gmail.com>
2025-02-12 19:32:24 +0100

    ex02

--- Python-4-Dod/ex02/callLimit.py
+++ Python-4-Dod/ex02/callLimit.py
@@ -1,5 +1,13 @@
 def callLimit(limit: int):
     count = 0
     def callLimiter(function):
-        def limit_function(*args: Any, **kwds: Any):
-            return "wip"
+        def limit_function(*args, **kwds):
+            nonlocal count
+            if count < limit:
+                count += 1
+                return function(*args, **kwds)
+            else:
+                print("Error: {} call too many times".format(function))
+        return limit_function
+    return callLimiter
+
