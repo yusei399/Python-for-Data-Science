84917b7379cddd0f1026e6e1e6a018fedcebdc9f
yusei399 <contact.ikechan@gmail.com>
2025-02-12 13:11:33 +0100

    fix/norm

--- Python-4-Dod/ex01/in_out.py
+++ Python-4-Dod/ex01/in_out.py
@@ -1,26 +1,27 @@
 def square(x: int | float) -> int | float:
     return x ** 2
 
+
 def pow(x: int | float) -> int | float:
     return x ** x
 
+
 def outer(x: int | float, function) -> object:
-        count = 0
-        try:
-            assert isinstance(x, (int, float)), "x must be int or float"
-            assert callable(function), "function must be callable"
+    count = 0
+    try:
+        assert isinstance(x, (int, float)), "x must be int or float"
+        assert callable(function), "function must be callable"
 
-            def inner() -> float:
+        def inner() -> float:
+            nonlocal x
+            nonlocal count
 
-                nonlocal x
-                nonlocal count
+            x = function(x)
+            count += 1
 
-                x = function(x)
-                count += 1
+            return x
 
-                return x
-
-            return inner
-        except AssertionError as e:
-            print("AssertionError:", e)
-            return None
+        return inner
+    except AssertionError as e:
+        print("AssertionError:", e)
+        return None
